63010c65054673a27d5f4d5e2658ceaf68fbfb91
Ali Hussain Alhammad <86885125+pamelafox101-ali@users.noreply.github.com>
2022-02-09 17:48:21 +0300

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,13 @@
+/** Alis list
+Milk (4)
+Peanut Butter (1)
+Dark Chocolate  (2)
+**/
+
+CREATE TABLE Ali (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER );
+
+INSERT INTO Ali VALUES (1, "Milk", 4);
+INSERT INTO Ali VALUES (2, "Peanut Butter", 1);
+INSERT INTO Ali VALUES (3, "Dark chocolate", 2);
+SELECT * FROM Ali;
+
